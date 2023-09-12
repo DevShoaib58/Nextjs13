3159c254e2f93b96a330993a8d96313214a00ad5
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-09-12 16:30:44 +0500

    Adding More New Section

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -4,8 +4,8 @@ import { services } from "../Constants";
 const Services = () => {
     return (
         <section className=" max-container flex justify-center flex-wrap gap-9">
-            {/* {services.map((item) => (<ServiceCard key={item.label} {...item} />)} */}
-            {services.map((item) => { return (<ServiceCard key={item.label} {...item} />) })}
+            {/* {services.map((item) => (<ServiceCard key={item.label} {...item} />))} */}
+            {services.map((item) => (<ServiceCard key={item.label} {...item} />))}
         </section>
     );
 };
--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,8 +1,27 @@
+import Button from "../Components/Button";
+import { offer } from "../assets/images";
+
 const SpecialOffer = () => {
     return (
-        <div>
-            SpecialOffer
-        </div>
+        <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1">
+                <img src={offer} width={773} height={687} className=" object-contain w-full" />
+            </div>
+            <div className="flex flex-1 flex-col">
+                <h1 className=" mt-10  font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+                    <span className=" text-coral-red inline-block mt-3">Special</span> Offer
+                </h1>
+                <p className="info-text font-montserrat text-slate-gray text-lg leading-8 mt-8 mb-4 sm:max-w-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesen </p>
+                <p className="info-text font-montserrat text-slate-gray text-lg leading-8 lg:max-w-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    harum quisquam eius sed odit fugiat iusto fuga praesen </p>
+                <div className="mt-11 flex flex-wrap gap-4">
+                    <Button className=" rounded border-r-8" label="View Details" />
+                    <Button className=" rounded border-r-8" label="View Details" />
+                </div>
+            </div>
+        </section>
     );
 };
 
